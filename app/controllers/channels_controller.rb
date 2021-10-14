8dd093652c411a7b5a624686b715335b0b11c84a
NickyEXE <dovern42@gmail.com>
2021-10-14 16:39:31 -0400

    remove serialization, add index

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -2,7 +2,12 @@ class ChannelsController < ApplicationController
 
   def show
     channel = Channel.find(params[:id])
-    render json: channel, include: [:messages]
+    render json: channel, only: [:id, :name],  include: {messages: {only: [:username, :message, :id]}}
+  end
+
+  def index
+    channels = Channel.all
+    render json: channels, only: [:id, :name]
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :messages, only: [:create, :destroy]
-  resources :channels, only: [:show]
+  resources :channels, only: [:show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
