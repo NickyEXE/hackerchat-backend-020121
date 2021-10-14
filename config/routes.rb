b0cadb29b027474ee17d52adc55650f6867382fb
NickyEXE <dovern42@gmail.com>
2021-10-14 17:23:00 -0400

    finished for lecture 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :messages, only: [:create, :destroy]
-  resources :channels, only: [:show, :index]
+  resources :messages, only: [:destroy]
+  resources :channels, only: [:show, :index] do
+    resources :messages, only: [:create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
